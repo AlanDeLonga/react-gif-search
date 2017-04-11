69b54856fa3366996fe152a4ccf7c133ff27f252
Alan <alan.delonga@gmail.com>
2017-04-11 10:18:08 -0700

    adding search with basic styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
+import request from 'superagent';
+import './styles/app.css';
 
 class App extends React.Component {
-  handleTermChange(term) {
-    console.log(term);
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+    };
+  }
+
+  handleTermChange = (term) => {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+
+    const self = this;
+
+    request.get(url, function(err, res) {
+      // console.log(res.body.data[0]);
+      self.setState({ gifs: res.body.data });
+    });
   }
 
   render () {
     return (
       <div className="greeting">
         <SearchBar onTermChange={this.handleTermChange} />
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }
