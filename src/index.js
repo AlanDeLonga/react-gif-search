c7252e33451016c8d4cbbc13f41aaad2df6349d6
Alan <alan.delonga@gmail.com>
2017-04-11 10:53:30 -0700

    setting up modal with click events

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList';
+import GifModal from './components/GifModal';
 import SearchBar from './components/SearchBar';
 import request from 'superagent';
 import './styles/app.css';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       gifs: [],
+      selectedGif: null,
+      modalIsOpen: false,
     };
+  }
+
+  openModal = (gif) => {
+    this.setState({
+      modalIsOpen: true,
+      selectedGif: gif,
+    });
+  }
+
+  closeModal = (gif) => {
+    this.setState({
+      modalIsOpen: false,
+      selectedGif: null,
+    });
   }
 
   handleTermChange = (term) => {
@@ -28,7 +45,11 @@ class App extends React.Component {
     return (
       <div className="greeting">
         <SearchBar onTermChange={this.handleTermChange} />
-        <GifList gifs={this.state.gifs} />
+        <GifList  gifs={this.state.gifs}
+                  onGifSelect={selectedGif => this.openModal(selectedGif)} />
+        <GifModal modalIsOpen={this.state.modalIsOpen}
+                  selectedGif={this.state.selectedGif}
+                  onRequestClose={ () => this.closeModal() } />
       </div>
     );
   }
